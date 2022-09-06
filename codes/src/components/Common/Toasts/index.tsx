559cc5391b40e038276d6456a506f3f982231dde
Charlie <charlie@lawsnote.com>
2022-09-06 17:41:10 +0800

    [perf] rename to Toasts

--- codes/src/components/Common/Toasts/index.tsx
+++ codes/src/components/Common/Toasts/index.tsx
@@ -1,15 +1,15 @@
 import React, { memo, useCallback } from 'react';
 import { css } from '@emotion/css';
 import Portal from '@Components/Layer/Portal';
-import Snack from '@Components/Common/Snack';
-import type { SnacksProps, Snacks } from './types';
+import Toast from '@Components/Common/Toast';
+import type { ToastsProps, ToastArray } from './types';
 
-function SnackMultiple(props: SnacksProps): React.ReactElement {
+function Toasts(props: ToastsProps): React.ReactElement {
   /* States */
-  const { snacks, onClose } = props;
+  const { toasts, onClose } = props;
 
   /* Functions */
-  const calcHeight = useCallback((array: Snacks, index: number): string => {
+  const calcHeight = useCallback((array: ToastArray, index: number): string => {
     const top = array.slice(0, index).filter((s) => s.show).length;
     return css({
       transform: `translateY(calc(${top * (60 + 24)}px))`,
@@ -29,22 +29,22 @@ function SnackMultiple(props: SnacksProps): React.ReactElement {
   return (
     <Portal>
       <React.Fragment>
-        {snacks.map((snack, index) => (
-          <Snack
+        {toasts.map((snack, index) => (
+          <Toast
             key={snack.id}
             show={snack.show}
             onClose={() => closeSingleSnack(snack.id)}
-            classes={{ snack: calcHeight(snacks, index) }}
+            classes={{ snack: calcHeight(toasts, index) }}
             countDown={snack.countDown}
             disableAutoClose={snack.disableAutoClose}
             pauseOnHover={snack.pauseOnHover}
           >
             {snack.children}
-          </Snack>
+          </Toast>
         ))}
       </React.Fragment>
     </Portal>
   );
 }
 
-export default memo(SnackMultiple);
+export default memo(Toasts);
