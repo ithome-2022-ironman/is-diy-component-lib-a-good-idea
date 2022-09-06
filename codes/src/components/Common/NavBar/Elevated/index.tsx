17e53106116fae6f4c857f5c7723b24554334bdc
Charlie <charlie@lawsnote.com>
2022-09-06 15:57:19 +0800

    [feat] add NavBarElevatedDemo

---
+++ codes/src/components/Common/NavBar/Elevated/index.tsx
@@ -0,0 +1,104 @@
+import React, { memo } from 'react';
+import { css } from '@emotion/css';
+import cn from 'classnames';
+import Container from '@Components/Layout/Container';
+import useScrollPercentage from '@Hooks/useScrollPercentage';
+
+const NAV_HEIGHT = 60;
+
+const navWrapper = css({
+  height: `${NAV_HEIGHT}px`,
+  width: '100%',
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  backgroundColor: '#283593',
+  filter: 'unset',
+  transition: 'filter .2s ease',
+});
+const navWrapperBgColor = css({
+  filter: 'drop-shadow(0 0 4px rgba(0, 0, 0, 0.4))',
+});
+const navStyle = css({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+const sectionOneWrapper = css({
+  height: '800px',
+  backgroundColor: '#6d4c41',
+});
+const sectionOneComponent = css({
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  padding: '48px',
+  '& div': {
+    flex: '1 1 auto',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+const sectionTwoWrapper = css({
+  height: '400px',
+  backgroundColor: '#9c786c',
+});
+const sectionThreeWrapper = css({
+  height: `calc(100vh - ${NAV_HEIGHT}px)`,
+  minHeight: '600px',
+  backgroundColor: '#40241a',
+});
+const footerWrapper = css({
+  height: '80px',
+  backgroundColor: '#6d4c41',
+});
+const footerComponent = css({
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+const whiteText = css({
+  color: '#fff',
+});
+
+function NavBarElevatedDemo(): React.ReactElement {
+  /* States */
+  const percentage = useScrollPercentage();
+
+  /* Main */
+  return (
+    <React.Fragment>
+      <Container
+        classes={{ wrapper: cn(navWrapper, percentage && navWrapperBgColor) }}
+        renderAs="nav"
+      >
+        <div className={cn(navStyle, whiteText)}>
+          <div>I am a top nav</div>
+        </div>
+      </Container>
+      <Container classes={{ wrapper: sectionOneWrapper }} disablePadding>
+        <div className={cn(whiteText, sectionOneComponent)}>
+          <div>section one left</div>
+          <div>section one right</div>
+        </div>
+      </Container>
+      <Container classes={{ wrapper: sectionTwoWrapper }}>
+        <div>section two</div>
+      </Container>
+      <Container classes={{ wrapper: sectionThreeWrapper }}>
+        <div className={cn(whiteText)}>section three</div>
+      </Container>
+      <Container classes={{ wrapper: footerWrapper }} renderAs="footer">
+        <div className={cn(whiteText, footerComponent)}>
+          here comes the footer
+        </div>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default memo(NavBarElevatedDemo);
