507eb9ce6dc0ca06f1f16f9472528abb58ee9873
Charlie <charlie@lawsnote.com>
2022-09-06 15:43:38 +0800

    [feat] add useIsScrollDown

---
+++ codes/src/hooks/useIsScrollDown.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+export default function useIsScrollDown(): boolean {
+  /* States */
+  const [oldScrollTop, setOldScrollTop] = useState<number>(0);
+  const [isDown, setIsDown] = useState<boolean>(false);
+
+  /* Functions */
+  const handleScrollDirection = (oldScrollTop: number) => () => {
+    const newScrollTop = window.scrollY || document.documentElement.scrollTop;
+    if (newScrollTop > oldScrollTop) {
+      setIsDown(true);
+    } else {
+      setIsDown(false);
+    }
+    setOldScrollTop(newScrollTop);
+  };
+
+  /* Hooks */
+  useEffect(() => {
+    window.addEventListener('scroll', handleScrollDirection(oldScrollTop));
+    return () => {
+      window.removeEventListener('scroll', handleScrollDirection(oldScrollTop));
+    };
+  }, [oldScrollTop]);
+
+  /* Main */
+  return isDown;
+}
