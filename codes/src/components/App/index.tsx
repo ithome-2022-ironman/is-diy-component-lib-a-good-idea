d065d5320aedaa04b2132e1953895889bc50588d
Tzu Yin <tzyn.wang@gmail.com>
2022-09-06 20:02:40 +0800

    [feat] demo basic structure

--- codes/src/components/App/index.tsx
+++ codes/src/components/App/index.tsx
@@ -1,17 +1,46 @@
 import React, { memo } from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import cn from 'classnames';
-import scopedStyle from './index.module.css';
+import { css } from '@emotion/css';
+import Link from '@Components/Common/Link';
+import Stack from '@Components/Layout/Stack';
 
-function App(): React.ReactElement {
+const appContainer = css({
+  height: '100%',
+  display: 'flex',
+  backgroundColor: '#f9f4ef',
+});
+const side = css({ height: '100%', flex: '1 1 30%' });
+const main = css({ height: '100%', flex: '1 1 70%' });
+
+function Routes(): React.ReactElement {
   /* Main */
   return (
-    <div className={cn(scopedStyle.main)}>
-      <div>Hello world, this site is created by</div>
-      <code className={cn(scopedStyle.mainCode)}>
-        npx create-react-app &lt;your-app-name&gt; template --choffee
-      </code>
-    </div>
+    <Router>
+      <div className={cn(appContainer)}>
+        <div className={cn(side)}>
+          <Stack>
+            <Link to="/">intro</Link>
+            <Link to="/01">day 1</Link>
+            <Link to="/02">day 2</Link>
+          </Stack>
+        </div>
+        <div className={cn(main)}>
+          <Switch>
+            <Route exact path="/">
+              <div>hello world</div>
+            </Route>
+            <Route path="/01">
+              <div>day 01</div>
+            </Route>
+            <Route path="/02">
+              <div>day 02</div>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
-export default memo(App);
+export default memo(Routes);
