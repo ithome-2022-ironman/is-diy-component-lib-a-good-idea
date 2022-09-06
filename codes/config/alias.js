b143bcc38e22d66f5eaf9aef94e6532702b2e2e4
Charlie <charlie@lawsnote.com>
2022-09-06 15:42:02 +0800

    [feat] update alias

--- codes/config/alias.js
+++ codes/config/alias.js
@@ -2,4 +2,5 @@ const path = require('path');
 
 module.exports = {
   '@Components': path.resolve(__dirname, '..', './src/components'),
+  '@Hooks': path.resolve(__dirname, '..', './src/hooks'),
 };
