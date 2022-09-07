184e2a52bf9661a135e1f67e9a596f8256197957
Tzu Yin <tzyn.wang@gmail.com>
2022-09-07 09:29:00 +0800

    [feat] implement day03

---
+++ codes/src/components/Page/day03/index.tsx
@@ -0,0 +1,21 @@
+import React, { memo } from 'react';
+import Typography from '@Components/Common/Typography';
+import Stack from '@Components/Layout/Stack';
+
+function Day02(): React.ReactElement {
+  return (
+    <Stack gap={0}>
+      <Typography renderAs="h1">H1</Typography>
+      <Typography renderAs="h2">H2</Typography>
+      <Typography renderAs="h3">H3</Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque rerum rem
+        sit laudantium, sapiente provident beatae modi magni veritatis eius
+        optio porro, est blanditiis exercitationem sint facere aspernatur, vel
+        maiores.
+      </Typography>
+    </Stack>
+  );
+}
+
+export default memo(Day02);
