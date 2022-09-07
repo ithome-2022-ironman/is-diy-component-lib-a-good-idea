6f2a17a395272c98500df1002a38b1402d899953
Charlie <charlie@lawsnote.com>
2022-09-07 16:01:11 +0800

    [feat] update assets/icons

--- codes/src/assets/icons/index.ts
+++ codes/src/assets/icons/index.ts
@@ -1,2 +1,4 @@
 export { ReactComponent as CloseIcon } from './close_black_24dp.svg';
+export { ReactComponent as ExpendLessIcon } from './expand_less_black_24dp.svg';
+export { ReactComponent as ExpendMoreIcon } from './expand_more_black_24dp.svg';
 export { ReactComponent as PhotoCameraIcon } from './photo_camera_black_24dp.svg';
