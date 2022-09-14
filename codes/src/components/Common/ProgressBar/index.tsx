0f249e8caa6ecba31f7d951e45cdf581d5dc5831
Tzu Yin <tzyn.wang@gmail.com>
2022-09-14 21:43:29 +0800

    [feat] update ProgressBar

--- codes/src/components/Common/ProgressBar/index.tsx
+++ codes/src/components/Common/ProgressBar/index.tsx
@@ -1,31 +1,54 @@
 import React, { memo, useMemo } from 'react';
 import { css } from '@emotion/css';
 import cn from 'classnames';
-import useScrollPercentage from '@Hooks/useScrollPercentage';
 import type { ProgressBarProps } from './types';
 
 function ProgressBar(props: ProgressBarProps): React.ReactElement {
   /* States */
-  const { height = 4, bg = 'linear-gradient(to right, #78909c, #4e342e)' } =
-    props;
+  const {
+    percentage,
+    height = 4,
+    width = '100%',
+    color = 'linear-gradient(to right, #78909c, #4e342e)',
+    bgColor = 'transparent',
+    classes = { bar: '', barBackground: '' },
+    barRounded = false,
+  } = props;
   const finalHeight = useMemo(() => (height ? Math.ceil(height) : 4), [height]);
-  const percentage = useScrollPercentage();
+  const baseStyle = useMemo(
+    () =>
+      css({
+        height: `${finalHeight}px`,
+        width,
+      }),
+    [width, finalHeight]
+  );
 
   /* Main */
   return (
     <div
       className={cn(
+        baseStyle,
         css({
-          height: `${finalHeight}px`,
-          width: '100%',
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          background: bg,
-          clipPath: `polygon(0 0, ${percentage}% 0, ${percentage}% 100%, 0 100%)`,
-        })
+          background: bgColor,
+        }),
+        classes.barBackground
       )}
-    />
+    >
+      <div
+        className={cn(
+          baseStyle,
+          css({
+            background: color,
+            clipPath: barRounded
+              ? `inset(0 ${100 - percentage}% 0 0 round ${finalHeight}px)`
+              : `polygon(0 0, ${percentage}% 0, ${percentage}% 100%, 0 100%)`,
+            transition: 'clip-path .2s ease',
+          }),
+          classes.bar
+        )}
+      />
+    </div>
   );
 }
 
--- codes/src/components/Common/ProgressBar/types.d.ts
+++ codes/src/components/Common/ProgressBar/types.d.ts
@@ -1,6 +1,25 @@
 import type { Property } from 'csstype';
 
 export interface ProgressBarProps {
+  percentage: number;
   height?: number;
-  bg?: (string | (string & {}))[] | Property.Background<string | number>;
+  width?:
+    | (string | (string & {}))[]
+    | Property.Width<string | number>
+    | NonNullable<Property.Width<string | number> | undefined>[];
+  color?:
+    | (string | (string & {}))[]
+    | Property.Background<string | number>
+    | NonNullable<Property.Background<string | number> | undefined>[];
+  bgColor?:
+    | (string | (string & {}))[]
+    | Property.Background<string | number>
+    | NonNullable<Property.Background<string | number> | undefined>[];
+  classes?: Partial<ProgressBarClasses>;
+  barRounded?: boolean;
+}
+
+interface ProgressBarClasses {
+  bar: string;
+  barBackground: string;
 }
