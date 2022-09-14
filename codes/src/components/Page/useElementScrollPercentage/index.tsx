d65b1e8e8f9f392da470c0d4d6f780bf9096e778
Tzu Yin <tzyn.wang@gmail.com>
2022-09-14 21:43:48 +0800

    [feat] update useElementIsScrollPercentageDemo

--- codes/src/components/Page/useElementScrollPercentage/index.tsx
+++ codes/src/components/Page/useElementScrollPercentage/index.tsx
@@ -3,28 +3,41 @@ import { css } from '@emotion/css';
 import cn from 'classnames';
 import SpaceWrapper from '@Components/Layout/SpaceWrapper';
 import Stack from '@Components/Layout/Stack';
+import ProgressBar from '@Components/Common/ProgressBar';
 import useElementScrollPercentage from '@Hooks/useElementScrollPercentage';
+
+const perspectiveLayer = css({
+  perspective: '99999px',
+});
+const fixedTop = css({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+});
+const boxWrapper = css({
+  height: '400px',
+  border: '1px solid #333',
+  overflow: 'auto',
+});
+const boxLong = css({ height: '200vh' });
 
 function useElementIsScrollPercentageDemo(): React.ReactElement {
   /* States */
   const divRef = useRef<HTMLDivElement | null>(null);
-  const percentage = useElementScrollPercentage(divRef);
+  const percentage = useElementScrollPercentage(divRef, 10);
 
   /* Main */
   return (
     <Stack>
       <SpaceWrapper padding={24}>
-        <div
-          className={cn(
-            css({
-              height: '400px',
-              border: '1px solid #333',
-              overflow: 'auto',
-            })
-          )}
-          ref={divRef}
-        >
-          <div className={cn(css({ height: '200vh' }))} />
+        <div className={cn(perspectiveLayer)}>
+          <ProgressBar
+            percentage={percentage}
+            classes={{ bar: fixedTop, barBackground: fixedTop }}
+          />
+        </div>
+        <div className={cn(boxWrapper)} ref={divRef}>
+          <div className={cn(boxLong)} />
         </div>
       </SpaceWrapper>
       <SpaceWrapper padding={[0, 24]}>
