4fbb917ad9c59570f7f89f5c45884f8b6251e304
Tzu Yin <tzyn.wang@gmail.com>
2022-09-16 19:46:24 +0800

    [feat] assets/icons add start icon

--- codes/src/assets/icons/index.ts
+++ codes/src/assets/icons/index.ts
@@ -5,3 +5,4 @@ export { ReactComponent as InsertPhotoIcon } from './insert_photo_black_24dp.svg
 export { ReactComponent as NavigateIcon } from './navigate_next_black_24dp.svg';
 export { ReactComponent as PersonIcon } from './person_black_24dp.svg';
 export { ReactComponent as PhotoCameraIcon } from './photo_camera_black_24dp.svg';
+export { ReactComponent as StarIcon } from './star_black_24dp.svg';
