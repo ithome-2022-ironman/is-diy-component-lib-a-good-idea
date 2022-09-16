d703a374175dbd1a3897c019f989b0375845558e
Tzu Yin <tzyn.wang@gmail.com>
2022-09-16 08:17:04 +0800

    [feat] update assets/icons

--- codes/src/assets/icons/index.ts
+++ codes/src/assets/icons/index.ts
@@ -1,5 +1,7 @@
 export { ReactComponent as CloseIcon } from './close_black_24dp.svg';
 export { ReactComponent as ExpendLessIcon } from './expand_less_black_24dp.svg';
 export { ReactComponent as ExpendMoreIcon } from './expand_more_black_24dp.svg';
+export { ReactComponent as InsertPhotoIcon } from './insert_photo_black_24dp.svg';
+export { ReactComponent as NavigateIcon } from './navigate_next_black_24dp.svg';
 export { ReactComponent as PersonIcon } from './person_black_24dp.svg';
 export { ReactComponent as PhotoCameraIcon } from './photo_camera_black_24dp.svg';
