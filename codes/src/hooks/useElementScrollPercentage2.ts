062575a45a8c0261c01a1c9623c3fc2b784230eb
Tzu Yin <tzyn.wang@gmail.com>
2022-09-16 08:17:40 +0800

    [feat] add useElementScrollPercentage2

---
+++ codes/src/hooks/useElementScrollPercentage2.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect, useCallback } from 'react';
+import { debounce } from 'lodash';
+
+export default function useElementScrollPercentage2<T extends Element>(
+  element: T | null,
+  delay: number = 100
+): number {
+  /* States */
+  const [percentage, setPercentage] = useState<number>(0);
+
+  /* Functions */
+  const calculatePercentage = useCallback((e: Event): void => {
+    const target = e.target as Element;
+    const { scrollTop, scrollHeight, clientHeight } = target;
+    const percent = (scrollTop / (scrollHeight - clientHeight)) * 100;
+    setPercentage(Math.round(percent));
+  }, []);
+  const debouncedCalcPercentage = debounce(calculatePercentage, delay);
+
+  /* Hooks */
+  useEffect(() => {
+    element?.addEventListener('scroll', debouncedCalcPercentage);
+    return () =>
+      element?.removeEventListener('scroll', debouncedCalcPercentage);
+  }, [element, debouncedCalcPercentage]);
+
+  /* Main */
+  return percentage;
+}
