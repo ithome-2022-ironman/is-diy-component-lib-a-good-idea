5cfaf4a95e1a89b235d9637941f004130f5a52d5
Tzu Yin <tzyn.wang@gmail.com>
2022-09-16 21:30:51 +0800

    [feat] update alias

--- codes/config/alias.js
+++ codes/config/alias.js
@@ -4,4 +4,5 @@ module.exports = {
   '@Assets': path.resolve(__dirname, '..', './src/assets'),
   '@Components': path.resolve(__dirname, '..', './src/components'),
   '@Hooks': path.resolve(__dirname, '..', './src/hooks'),
+  '@Tools': path.resolve(__dirname, '..', './src/tools'),
 };
