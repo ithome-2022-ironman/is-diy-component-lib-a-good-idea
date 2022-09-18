9504cbb5f6fb270f7c8ac219c4d8efadb1282194
Tzu Yin <tzyn.wang@gmail.com>
2022-09-18 20:03:47 +0800

    [feat] remove Button/UploadButton

--- codes/src/components/Common/Button/UploadButton/types.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import type { ButtonProps } from '@Components/Common/Button/types';
-
-export interface UploadButtonProps extends ButtonProps {
-  icon?: React.ReactNode;
-  iconPosition?: 'left' | 'right';
-}
